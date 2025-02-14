50f677387eb063e3a46e90b3c00a8831e8fb6ae3
nameisnamish <namishmsasmyselfmein2033@gmail.com>
2025-02-14 10:37:22 +0530

    LAB 4
    PPDA

---
+++ LAB 4/Dictionary_All_Functions.py
@@ -0,0 +1,51 @@
+#Example
+Mobile = {
+    "brand" : "Samsung",
+    "model" : "S10",
+    "Screen size" : 6.1
+}
+print(Mobile)
+
+#Accessing
+X = Mobile["model"]
+Y = Mobile.get("model")
+print(X)
+print(Y)
+
+#Modifing/ Updating exixting value
+Mobile["model"] = "S11"
+print(Mobile)
+
+#Adding new element:
+Mobile["color"] = 'blue'
+print(Mobile)
+
+#Deleting:
+del Mobile["color"]
+print(Mobile)
+
+#Remove last inserted item
+Mobile.popitem()
+
+#Lopping through a directory
+#Print all keys
+for key in Mobile:
+    print(key)
+#Print all values
+for key in Mobile:
+    print(Mobile[key])
+
+#Dictionary Functions: dict.values(),dict.items()
+#Print all values
+for value in Mobile.values():
+    print(value)
+#Print keys ans values 
+for key, value in Mobile.items():
+    print(key,value)
+
+#checking key existance
+if "model" in Mobile:
+    print("Yes, 'model' is a key in Mobile dictionary")
+
+#Copying a directory
+Mobilephone = Mobile.copy()
