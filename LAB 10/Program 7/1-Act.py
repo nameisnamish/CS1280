c713df545f17fafd1ec9550dc728e5592ceb8b01
nameisnamish <namishmsasmyselfmein2033@gmail.com>
2025-04-04 10:00:28 +0530

    Update 1-Act.py

--- LAB 10/Program 7/1-Act.py
+++ LAB 10/Program 7/1-Act.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+data = [
+    {"Product Name": "Laptop", "Category": "Electronics", "Price": 50000},
+    {"Product Name": "Chair", "Category": "Furniture", "Price": 3000},
+    {"Product Name": "Notebook", "Category": "Stationery", "Price": 100},
+    {"Product Name": "Smartphone", "Category": "Electronics", "Price": 20000},
+]
+
+df = pd.DataFrame(data)
+
+df["Discount Price"] = df["Price"] * 0.9
+
+print(df)
